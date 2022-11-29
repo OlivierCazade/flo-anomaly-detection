0db0010de69448a58899e7d3154025d059a38230
Olivier Cazade <ocazade@redhat.com>
2022-11-29 16:30:19 +0100

    Second try with different model for each pod

--- main.py
+++ main.py
@@ -23,18 +23,31 @@ def create_consumer(server, topic, group_id):
 
     return consumer
 
+models = {}
+
+def checkRecordForPod(pod, record):
+    #Memory leak here since we never address old models
+    model = models.get(pod, compose.Pipeline(
+            preprocessing.FeatureHasher(),
+            preprocessing.MinMaxScaler(),
+            anomaly.HalfSpaceTrees(n_trees=10,
+                                   height=10,
+                                   window_size=250)
+        )
+)
+
+    model = model.learn_one(record)
+    score = model.score_one(record)
+    if score > 0.8:
+        print("======================================")
+        print(record)
+        print(f'Pod: {pod} Anomaly score: {score:.3f}')
+    models[pod] = model
+
 def detect():
     consumer = create_consumer(server="kafka-cluster-kafka-bootstrap.netobserv",
                                topic="netobserv-flows-export",
                                group_id="test")
-
-    model = compose.Pipeline(
-        preprocessing.FeatureHasher(),
-        preprocessing.MinMaxScaler(),
-        anomaly.HalfSpaceTrees(n_trees=8,
-                               height=8,
-                               window_size=100)
-    )
     while True:
         message = consumer.poll(timeout=50)
         if message is None:
@@ -42,15 +55,15 @@ def detect():
         if message.error():
             logging.error("Consumer error: {}".format(message.error()))
             continue
-
-        # Message that came from producer
         record = json.loads(message.value().decode('utf-8'))
-        model = model.learn_one(record)
-        score = model.score_one(record)
-        if score > 0.8:
-            print("======================================")
-            print(record)
-            print(f'Anomaly score: {score:.3f}')
+        if "Duplicate" in record and record["Duplicate"] is True :
+            continue
+        if "SrcK8S_Name" in record:
+            if "SrcK8S_OwnerType" in record and record["SrcK8S_Type"] == "Pod":
+                checkRecordForPod(record["SrcK8S_Name"], record)
+        if "DstK8S_Name" in record:
+            if "DstK8S_OwnerType" in record and record["DstK8S_Type"] == "Pod":
+                checkRecordForPod(record["DstK8S_Name"], record)
 
     consumer.close()
 
